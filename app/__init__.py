8760aeca457c591fbe23f5e188327a8b38432724
dxncat <xidarkmoon@gmail.com>
2024-02-08 16:01:16 -0500

    primer commit, falta agregar pqgina de error 404 entre otros

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+#importar flask
+from flask import Flask, render_template, request, flash
+from requests import get
+from json import loads
+
+#configurar la app
+app = Flask(__name__)
+
+#configurar rutas
+@app.route('/hola')
+def saludo():
+    return  'hola parce'
+
+@app.route('/search', methods=['GET', 'POST'])
+def paises():
+    print(request.method)
+    #definir lista de paises
+    if request.method == 'POST':
+        try:
+            personajes = loads(get(f'https://rickandmortyapi.com/api/character/?name={request.form["name"]}').text)['results']
+            return render_template('mm.html', personajes=personajes)
+        except:
+            flash('PERSONAJE NO ENCONTRADO!')
+            return render_template('mm.html')
+    else:
+        return render_template('search.html')
+
+
+@app.route('/')
+def home():
+    personajes = loads(get('https://rickandmortyapi.com/api/character').text)
+    return render_template('index.html', personajes = personajes['results'])
+
+if __name__ == '__main__':
+    app.run()
